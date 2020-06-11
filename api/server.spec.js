f542ec26079047cca9afeb6925b48f8b7dcc608f
Alex Magana <46731475+jamcoding@users.noreply.github.com>
2020-06-10 21:56:22 -0500

    practice

---
+++ api/server.spec.js
@@ -0,0 +1,36 @@
+const request = require('supertest');
+const server = require('./server.js');
+
+describe('server.js', () => {
+    it('should set the testing envrionment', () => {
+        expect(process.env.DB_ENV).toBe('testing');
+    });
+
+    describe('GET /', () =>{
+        // return code
+        // return type of data
+        // boyd shape/structure
+
+        it('should return 200 ok', () => {
+            return request(server).get('/')
+                .then(res => {
+                    expect(res.status).toBe(200);
+                });
+        });
+
+        // this is the same as above
+        it('should return 200 ok using async/await', async () => {
+            const res = await request(server).get('/');
+            expect(res.status).toBe(200);
+        });
+
+        it('should return {api: "up"}', async () => {
+            const res = await request(server).get('/');
+            expect(res.body).toEqual({api: "up"});
+        });
+    });
+});
+
+describe('test.db3', () => {
+    
+});
